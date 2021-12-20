3ed8178f77aa73ad6ee8c373dea479a083e2c00e
bonot9434 <bonot9434@gmail.com>
2021-12-20 19:28:57 +0900

    Sample06

---
+++ Sample06.java
@@ -0,0 +1,33 @@
+class Sample06 {
+  public static void main(String[] args) {
+    int numberX = 8;
+    int numberY = 8;
+    int numberZ = 8;
+    char chNumberX = 'あ';
+    char chNumberY = 'あ';
+
+    boolean logicalValue1 = numberX == numberY;
+    System.out.println("logicalValue1:" + logicalValue1);
+
+    boolean logicalValue2 = numberX != numberY;
+    System.out.println("logicalValue2:" + logicalValue2);
+
+    boolean logicalValue3 = numberX > numberY;
+    System.out.println("logicalValue3:" + logicalValue3);
+
+    boolean logicalValue4 = numberX >= numberY;
+    System.out.println("logicalValue4:" + logicalValue4);
+
+    boolean logicalValue5 = numberX < numberY;
+    System.out.println("logicalValue5:" + logicalValue5);
+
+    boolean logicalValue6 = numberX <= numberY;
+    System.out.println("logicalValue6:" + logicalValue6);
+
+    boolean logicalValue7 = chNumberX == chNumberY;
+    System.out.println("logicalValue7:" + logicalValue7);
+
+    boolean logicalValue8 = logicalValue1 == logicalValue2;
+    System.out.println("logicalValue8:" + logicalValue8);
+  }
+}
