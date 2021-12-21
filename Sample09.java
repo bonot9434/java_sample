9444ef88d7848867263e29f9bc931a72bda3e5a6
bonot9434 <bonot9434@gmail.com>
2021-12-21 14:50:02 +0900

    Sample09更新

--- Sample09.java
+++ Sample09.java
@@ -1,11 +1,11 @@
 class Sample09 {
   public static void main(String[] args) {
-    int [][] rooms = {{101, 102, 103}, {201, 202, 203}, {301, 302, 303}};
+    int [][] rooms = {{101, 102, 103, 104}, {201, 202, 203}, {301, 302, 303}};
 
-    System.out.println("room[1][2]:" + rooms[1][2]);
+    System.out.println("rooms[1][2]:" + rooms[1][2]);
 
-    System.out.println("room.length:" + rooms.length);
+    System.out.println("rooms.length:" + rooms.length);
 
-    System.out.println("room[1].length:" + rooms[1].length);
+    System.out.println("rooms[0].length:" + rooms[0].length);
   }
 }
