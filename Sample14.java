9d734cf261de26fdae2786cfeca5bc881fb2cc27
bonot9434 <bonot9434@gmail.com>
2021-12-21 23:40:02 +0900

    Sample14

---
+++ Sample14.java
@@ -0,0 +1,15 @@
+class Sample14 {
+  public static void main(String[] args) {
+    int print = sum(2, 3);
+
+    System.out.println("print:" + print);
+  }
+
+  static int sum(int num1, int num2) {
+    int calcResult = num1 + num2;
+
+    return calcResult;
+  }
+}
+
+class Sample14_1{}
