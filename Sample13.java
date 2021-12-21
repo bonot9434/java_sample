065efc7d6a50b5cec3aca6c7ea3d7238aa6fae90
bonot9434 <bonot9434@gmail.com>
2021-12-21 23:27:40 +0900

    Sample13

---
+++ Sample13.java
@@ -0,0 +1,9 @@
+class Sample13 {
+  public static void main(String[] args) {
+    String[] animals = {"こぶた", "たぬき", "きつね", "ねこ"};
+
+    for (int i=0; i<animals.length; i++) {
+      System.out.println("animals[i]:" + animals[i]);
+    }
+  }
+}
